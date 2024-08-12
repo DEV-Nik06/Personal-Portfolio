f52b8b2f8b53bfba122dfd7b430ee830980488a3
unknown <yadavgr306@gmail.com>
2024-08-12 19:56:35 +0530

    final commit

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-
+//text animation
+var typed = new Typed("#Name", {
+    strings: ['Nikhil',' And a WEB DEVLOPER', 'And a WEB DESIGNER', 'And a VIDEO EIDTOR'],
+    typeSpeed: 150,loop: true
+  });
+  //
 let active = document.querySelectorAll('.value');
 active.forEach(navLinks=>{
     navLinks.addEventListener('click',function(e){
         document.querySelector('.active').classList.remove('active');
         navLinks.classList.add('active');
+        document.querySelector('.open').classList.remove('open');
     })
 })
-
+//
 let hamburger = document.getElementById('btn-ham');
 let navbar = document.getElementById('nav');
 hamburger.addEventListener('click',onHamClick);
-
+let logo = document.querySelector('.left-nav');
 function onHamClick(e) {
     if (!navbar.classList.contains('open')) {
         
-        navbar.classList.add('open');
+        navbar.classList.toggle('open');
+        logo.classList.add('img')
     }else{
         navbar.classList.remove('open');
     }
 }
+
+
