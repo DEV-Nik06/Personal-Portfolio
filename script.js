2eaffe371a2247aae5dc106675609171157ef123
unknown <yadavgr306@gmail.com>
2024-09-25 18:38:30 +0530

    Update

--- script.js
+++ script.js
@@ -1,38 +1,31 @@
 //text animation
 var typed = new Typed("#Name", {
-    strings: ['A WEB DEVLOPER', 'A WEB DESIGNER', 'And a VIDEO EIDTOR'],
-    typeSpeed: 120,loop: true
-  });
-  //
-let active = document.querySelectorAll('.value');
-active.forEach(navLinks=>{
-    navLinks.addEventListener('click',function(e){
-        document.querySelector('.active').classList.remove('active');
-        navLinks.classList.add('active');
-        document.querySelector('.open').classList.remove('open');
-    })
-})
+  strings: ["A WEB DEVLOPER", "A WEB DESIGNER", "And a VIDEO EIDTOR"],
+  typeSpeed: 120,
+  loop: true,
+});
 //
-const hamburgerBtn = document.getElementById('btn-ham');
-const navbar = document.getElementById('nav');
+let active = document.querySelectorAll(".value");
+active.forEach((navLinks) => {
+  navLinks.addEventListener("click", function (e) {
+    document.querySelector(".active").classList.remove("active");
+    navLinks.classList.add("active");
+    document.querySelector(".open").classList.remove("open");
+  });
+});
+//
+const hamburgerBtn = document.getElementById("btn-ham");
+const navbar = document.getElementById("nav");
 
-hamburgerBtn.addEventListener('click', () => {
-  navbar.classList.toggle('open'); // Toggle open/close class on the navbar
+hamburgerBtn.addEventListener("click", () => {
+  navbar.classList.toggle("open"); // Toggle open/close class on the navbar
 });
 
+let check = document.querySelector("#check-inp");
+check.addEventListener("click", function (e) {
+  check.classList.add("checked");
 
-
-
-let check = document.querySelector('#check-inp');
-check.addEventListener('click',function(e){
-    check.classList.add('checked')
-
-
-    if (check.classList.contains("checked")) {
-        document.body.classList.toggle("dark-theme");
-        
-    } 
-
-    
-
-})
+  if (check.classList.contains("checked")) {
+    document.body.classList.toggle("dark-theme");
+  }
+});
